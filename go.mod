b4f46ce0fb6bfe15b03f48c5aced786f38715d70
fikri fadillah <fikrifadillah231@gmail.com>
2022-08-13 08:43:22 +0700

    creating apps CalculateMath

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/guancang10/Apps-CalculateMath
+
+go 1.19
+
+require github.com/guancang10/CalculateMath/v2 v2.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	calculatemath "github.com/guancang10/CalculateMath/v2"
+)
+
+func main() {
+	fmt.Println(calculatemath.SquareRoot(5, 6))
+	fmt.Println(calculatemath.Factorial(5))
+}
